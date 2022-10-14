e1c5d3606908e37fec308ea41161223819053e6d
Ezra-Mallo <ezra.mallo@gmail.com>
2022-10-14 03:22:04 +0100

    Tasks 2 to 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/** The first program  */
+
+/**
+ * puts - print a string test.
+ * @void: takes no arguement
+ *
+ * Description: Prints the text
+ * Returns: returns 0
+ */
+void puts(void)
+{
+	printf"Programming is like building a multilingual puzzle\n";
+	return (0);
+}
+
+
+/**
+ * main - is the main entry point
+ * @void: Takes no argument
+ *
+ * Description: This is the main entry. It calls the put function
+ * to print the text
+ * Return: return 0 to show code has finished execution
+ */
+int main(void)
+{
+	/** call the puts */
+	puts();
+	return (0);
+}

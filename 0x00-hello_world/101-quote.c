4671795ed0089634e9b0e04eec63a232c9a3bb26
Ezra-Mallo <ezra.mallo@gmail.com>
2022-10-14 17:23:43 +0100

    Task 7 to 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,7 +12,7 @@ int write(int filedes, const void *buf, unsigned int nbyte);
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful", 30);
-	write(1, "- Dora Korpar, 2015-10-19\n", 30);
+	write(1, "and that piece of art is useful", 31);
+	write(1, "- Dora Korpar, 2015-10-19\n", 25);
 	return (1);
 }

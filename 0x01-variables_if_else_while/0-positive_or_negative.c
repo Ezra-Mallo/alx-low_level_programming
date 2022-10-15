31fe022d57e1eca7c412619dc936999515f6f725
Ezra-Mallo <ezra.mallo@gmail.com>
2022-10-15 02:45:54 +0100

    Tasks 0-1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,11 +6,12 @@
 
 
 /**
- * Main - The function is the entry point
+ * main - The function is the entry point
  * @void: is the arguement and it returns 0
  *
- * Description: Using betty style doc this is to test my control 
+ * Description: Using betty style doc this is to test my control
  * structure knowledge
+ * return: returns 0
  */
 int main(void)
 {

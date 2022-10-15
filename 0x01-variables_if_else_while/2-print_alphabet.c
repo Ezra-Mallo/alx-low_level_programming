a7f4f6df9427c525d2774660b0a926d91eb553f4
Ezra-Mallo <ezra.mallo@gmail.com>
2022-10-15 15:40:05 +0100

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/* this code will print a to z in lower case */
+/* this code will print a to z in lower cases. */
 
 /**
  * main - This is the main entry to the program
@@ -8,16 +8,20 @@
  *
  * Desciption: This program prints the alphabets in alphabetical order
  * Return: returns zero
+ * ascii_char were used t decode the lowecases
  */
 int main(void)
 {
 	int ascii_char = 97;
 	int i = 0;
 
-	while (i <= 26)
+	while (i < 26)
 	{
 		putchar (ascii_char + i);
 		i++;
 	}
+
+	putchar ('\n');
+
 	return (0);
 }

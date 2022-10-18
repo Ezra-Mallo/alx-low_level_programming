4d8aa0700cd83ea7f4869b996b33e0bc112b617e
Ezra-Mallo <ezra.mallo@gmail.com>
2022-10-18 19:02:14 +0100

    Task 0 and 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,14 +4,15 @@
 /**
  * print_alphabet - makes a call to _putchar to print alphabets
  * @void: no parameter passed
- * 
+ *
  *Return:  returns 0
  */
 
 void print_alphabet(void)
 {
 	int i;
-	for(i ='a'; i <= 'z'; i++)
+
+	for (i = 'a'; i <= 'z'; i++)
 		_putchar(i);
 	_putchar('\n');
 }

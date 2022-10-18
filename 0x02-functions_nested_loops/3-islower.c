36de3f534fa91d631d101d714baad12f5351ed19
Ezra-Mallo <ezra.mallo@gmail.com>
2022-10-18 22:08:08 +0100

    Task 3 to 4

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,7 +5,10 @@
  * islower - this is a function that returns 1 or 0 if the it receives xter
  * @c: is the parameter
  *
+ * Description: This function return 1 if lower case character is entered
+ *              and returned 0 if otherwise
  * Return: return 0 or 1
+ *
  */
 int _islower(int c)
 {
@@ -13,7 +16,7 @@ int _islower(int c)
 
 	if (c >= 97 && c <= 122)
 		result = 1;
-	else 
+	else
 		result = 0;
 
 	return (result);

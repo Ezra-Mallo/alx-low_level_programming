ca23d7450dca3f263892b62d53d5d688780c1af3
Ezra-Mallo <ezra.mallo@gmail.com>
2022-10-18 14:12:20 +0100

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * mainr - prints the _putchar to stdout
+ * @void: No parameter
+ *
+ * Return: On success returns 0.
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is the readme file for all task realted to nested lops

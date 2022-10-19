53f120a79c08b993adf1be4444941907e0db442d
Ezra-Mallo <ezra.mallo@gmail.com>
2022-10-19 01:32:15 +0100

    Task 4 to 5

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -11,10 +11,10 @@ int _isalpha(int c)
 {
 	int result;
 
-	if (c >= 65 && c <= 90 && c <= 97 && c <= 122)
-		result = 0;
-	else
+	if ((c >= 65 && c <= 90) || (c <= 97 && c <= 122))
 		result = 1;
+	else
+		result = 0;
 
 	return (result);
 }

ba568ea8570dc408e9d36a7d9c670fc5b35df775
Ezra-Mallo <ezra.mallo@gmail.com>
2022-10-21 00:39:27 +0100

    Task 4 to 4

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers -  to print numbers
+ * @void: The digit tp print
+ * Return: noting
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10;  i++)
+		_putchar(i + '0');
+
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,3 +3,5 @@ int _isupper(int c);
 int _isdigit(int);
 int mul(int a, int b);
 void _put_many_chars(int n);
+void print_numbers(void);
+void print_most_numbers(void);

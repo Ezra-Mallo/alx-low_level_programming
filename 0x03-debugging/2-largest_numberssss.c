3c0c6e50c6dd8e46aa235400039226f2d136089b
Ezra-Mallo <ezra.mallo@gmail.com>
2022-10-20 14:36:31 +0100

    Task 0 to 3

--- 0x03-debugging/2-largest_numberssss.c
+++ 0x03-debugging/2-largest_numberssss.c
@@ -8,16 +8,22 @@
  * Description: this processes the largest of 3 numbers and returns it.
  * Return: largest number
  */
+
 int largest_number(int a, int b, int c)
 {
 	int largest;
 
 	if (a > b && a > c)
+	{
 		largest = a;
+	}
 	else if (b > a && b > c)
+	{
 		largest = b;
+	}
 	else
+	{
 		largest = c;
-
+	}
 	return (largest);
 }

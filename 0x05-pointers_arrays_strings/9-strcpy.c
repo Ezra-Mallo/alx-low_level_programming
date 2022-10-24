58a3df0ffebb230a3a24777e71e2a5bd15b3a97a
Ezra-Mallo <ezra.mallo@gmail.com>
2022-10-24 23:48:03 +0100

    Task 8 - a function that prints n elements of an array of integers, followed by a new line. Task 9 -Write a function that copies the string pointed to by src, including the terminating null byte (\0), to the buffer pointed to by dest.

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * *_strcpy - kasjkajksjaasjkajkjksa
+ * @dest: First parameter is the pointer for the destination
+ * @src: Second parameter is the pointer to the source
+ *
+ * Return: Character pointer
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char * c;
+
+	c = *src;
+
+	return (c);
+}

08807ada8956feaa43e7358acfa14479f4beb6d9
Ezra-Mallo <ezra.mallo@gmail.com>
2022-10-24 19:56:36 +0100

    Task 2-a function that returns the length of a string. Task 3 -a function that prints a string, followed by a new line, to stdout.

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,14 +4,14 @@
  * _puts - a function that returns the length of a string
  * @str: first parameer. a char data type
  * Return: return an integer value
- **/
+ */
 void _puts(char *str)
 {
 	int count;
 	char null_char;
 	int i;
 
-	null_char = ' ';
+	null_char = 'x';
 	count = 0;
 
 	while (null_char != '\0')
@@ -20,7 +20,7 @@ void _puts(char *str)
 		count = count + 1;
 	}
 
-	for (i = 0; i < count; i++)
+	for (i = 0; i < count-1; i++)
 		_putchar(*(str + i));
 	_putchar('\n');
 

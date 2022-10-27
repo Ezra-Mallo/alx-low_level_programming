fedaa14dda7d6f8d407880ef9f8c6f552e42d2a1
Ezra-Mallo <ezra.mallo@gmail.com>
2022-10-27 21:39:11 +0100

    Task 6 - Write a function that capitalizes all words of a string. Task 7 - Write a function that encodes a string into 1337.

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,11 +2,9 @@
 #include <stdio.h>
 /**
  * leet - Write a function that encodes a string into 1337
- *		Letters a and A should be replaced by 4
- *		Letters e and E should be replaced by 3
- *		Letters o and O should be replaced by 0
- *		Letters t and T should be replaced by 7
- *		Letters l and L should be replaced by 1
+ *	Letters a & A replaced by 4, Letters e & E replaced by 3
+ *	Letters o & O replaced by 0, Letters t & replaced by 7
+ *	Letters l & L should be replaced by 1
  * @a: First parameter is the pointer for the destination
  *
  * Return: returns a pointer
@@ -24,26 +22,26 @@ char *leet(char *a)
 		nul = a[k];
 		switch (nul)
 		{
-			case 65:
-			case 97:
-				a[k] = '4' + 0;
-				break;
-			case 69:
-			case 101:
-				a[k] = '3' + 0;
-				break;
-			case 79:
-			case 111:
-				a[k] = '0' + 0;
-				break;
-			case 84:
-			case 116:
-				a[k] = '7' + 0;
-				break;
-			case 76:
-			case 108:
-				a[k] = '1' + 0;
-				break;
+		case 65:
+		case 97:
+			a[k] = '4' + 0;
+			break;
+		case 69:
+		case 101:
+			a[k] = '3' + 0;
+			break;
+		case 79:
+		case 111:
+			a[k] = '0' + 0;
+			break;
+		case 84:
+		case 116:
+			a[k] = '7' + 0;
+			break;
+		case 76:
+		case 108:
+			a[k] = '1' + 0;
+			break;
 		}
 		k++;
 	}

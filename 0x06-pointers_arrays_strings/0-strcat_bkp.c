fb7ef909a62447089af6ddb3d1eb760dbeeea44c
Ezra-Mallo <ezra.mallo@gmail.com>
2022-10-27 12:40:54 +0100

    Task 0, 1 & 2-Write a function that concatenates two strings.

--- 0x06-pointers_arrays_strings/0-strcat_bkp.c
+++ 0x06-pointers_arrays_strings/0-strcat_bkp.c
@@ -9,7 +9,25 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	strcat(dest, src);
+	/*int i, j, k, li;*/
 
+	/*i = _strlen(dest);*/
+	/*j = _strlen(src) + 1;*/
+
+	strcat(*dest, *src);
+	/**
+	 * l = 0;
+	for (k = i; k <= (i + j); k++)
+	{
+		if ((*(src + l)) != '\0')
+			dest[k] = *(src + l);
+		else if ((*(src + l)) != '\n')
+			dest[k] = '\n';
+		else if ((*(src + l)) == '\0')
+			dest[k] = '\0';
+
+		l++;
+	}
+	 */
 	return (dest);
 }

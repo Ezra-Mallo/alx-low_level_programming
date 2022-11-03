0518d6042f836ba9e090eda60577653ad2fd3dcd
Ezra-Mallo <ezra.mallo@gmail.com>
2022-11-03 01:08:11 +0100

    Task 2- Write a function that returns the length of a string. Task 3- Write a function that returns the factorial of a given number. Task 4- Write a function that returns the value of x raised to the power of y.

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -16,7 +16,7 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	else if (y == 0)
 		return (1);
-	else
-		r = x * _pow_recursion(x , y - 1);
+	
+	r = x * _pow_recursion(x , y - 1);
 	return (r);
 }

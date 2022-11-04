7d392e2a86127ea71d83d8c44dd9fe70fceaf9eb
Ezra-Mallo <ezra.mallo@gmail.com>
2022-11-04 12:32:46 +0100

    Task 0- Write a program that prints its name, followed by a new line. Task 1- Write a program that prints the number of arguments passed into it. Task 2- Write a program that prints all arguments it receives. Task 3- Write a program that multiplies two numbers. Task 4- Write a program that adds positive numbers.

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,7 +3,7 @@
 /**
  * main - This is the main entry to my program
  * @argc: First parameter that indicated the count of the arguement
- * @iargv: Second parameer that indicated the string arguement
+ * @*argv: Second parameer that indicated the string arguement
  * Return: 0 to indicate succeful execution
  */
 int main(int argc, char *argv[])
@@ -12,9 +12,10 @@ int main(int argc, char *argv[])
 
 	for (i = 0; i < argc; i++)
 	{
-		printf("%s\n", argv[i]);
+		printf("%s", argv[i]);
 		if (i == 0)
 			break;
 	}
+	printf("\n");
 	return (0);
 }

68cc3b4331ba907e9c2ca78f606c462e0c331100
Ezra-Mallo <ezra.mallo@gmail.com>
2022-11-09 00:03:40 +0100

    Task 2- update on concatenation

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,16 +13,16 @@ char *create_array(unsigned int size, char c)
 {
 	int i = 0;
 	char *a;
-	int b = size * sizeof(c) + 1;
+	int b = size * sizeof(c);
 
 	if (size == 0)
 		return (NULL);
 
-	a = malloc(size * sizeof(c) + 1);
+	a = malloc(size * sizeof(c));
 
 	for (i = 0; i < b; i++)
 		a[i] = c;
-	a[b - 1] = '\0';
+	a[b] = '\0';
 
 	if (a == NULL)
 		return (NULL);

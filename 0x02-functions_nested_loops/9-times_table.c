25d4cc692ef581cf160df1d1c946ec002d6bca65
Ezra-Mallo <ezra.mallo@gmail.com>
2022-11-12 22:05:39 +0100

    Task 8 updated

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,7 +20,8 @@ void times_table(void)
 
 			if (r1 < 10)
 			{
-				_putchar(' ');
+				if (j>0)
+					_putchar(32);
 				_putchar(r2 + '0');
 			}
 			else if (r1 >= 10)
@@ -32,8 +33,8 @@ void times_table(void)
 			if (j < 9)
 				_putchar(','), _putchar(' ');
 
-			/** if (result < 10)
-				_putchar(' ');*/
+			if (j > 10)
+				_putchar(' ');
 		}
 		_putchar('\n');
 	}

0f92ee1137162880d9fd452a5914bc9c0254b5aa
Ezra-Mallo <ezra.mallo@gmail.com>
2022-11-12 11:22:27 +0100

    Update task 8-base 16, and Task 12-combination of numbers

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,40 +1,20 @@
 #include <stdio.h>
 
-/* this code will print a to z in lower cases. */
-
 /**
  * main - This is the main entry to the program
  * @void: the main program takses no argument
- *
  * Desciption: This program prints the single digits of base 10
  * Return: returns zero
  */
 int main(void)
 {
 	int i;
-	int ch;
 
-	while (i < 16)
-	{
-		if (i == 10)
-			ch = 'a';
-		else if (i == 11)
-			ch = 'b';
-		else if (i == 12)
-			ch = 'c';
-		else if (i == 13)
-			ch = 'd';
-		else if (i == 14)
-			ch = 'e';
-		else if (i == 15)
-			ch = 'f';
-		else
-			putchar(i + '0');
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
 
-		putchar(ch);
-		i++;
-	}
-
+	for (i = 'a'; i <= 'f'; i++)
+		putchar(i);
 	putchar('\n');
 	return (0);
 }

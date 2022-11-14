20af602d84760d73cf16fd99076df61dc4d0b386
Ezra-Mallo <ezra.mallo@gmail.com>
2022-11-14 15:46:23 +0100

    Task 0: Object-like Macro. Create a header file that defines a macro named SIZE as an abbreviation for the token 1024. Task 1.1: Pi. Create a header file that defines a macro named PI as an abbreviation for the token 3.14159265359. Task 2: File name. Write a program that prints the name of the file it was compiled from, followed by a new line.

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,7 @@
+#ifndef _MAIN_HEAD_
+#define _MAIN_HEAD_
+
+#define PI 3.14159265359
+
+
+#endif
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define PNAME __FILE__
+
+int main(void)
+{
+/*	int i;
+
+	for (i = 0 ; i <= argc; i++)
+	{
+		if (i <= 0)
+			printf("%s", argv[0]);
+	}*/
+
+	printf("%s\n", PNAME);
+
+	return (0);
+}

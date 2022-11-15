379a7e35e2c100e120305bfdf9719b4c0bf99729
Ezra-Mallo <ezra.mallo@gmail.com>
2022-11-15 04:22:59 +0100

    Task 0: Define a new type struct dog with the following elements: name, type = char *, age, type = float, owner, type = char *

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef __MAIN_HEAD__
 #define __MAIN_HEAD__
-
+/**
+ * struct dog - This is the struct
+ * @name: First parameter
+ * @age: Second parameter
+ * @owner: Third parameter
+ */
 struct dog
 {
 	char *name;

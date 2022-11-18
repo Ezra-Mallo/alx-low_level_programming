6ed1b04db269aa5e604ea0ecb07ed6287ab30f4b
Ezra-Mallo <ezra.mallo@gmail.com>
2022-11-18 13:35:38 +0100

    Task 3: Write a program that performs simple operations. Usage: calc num1 operator num2.

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -4,22 +4,39 @@
  * main - Main entry point
  * @argc: This is the first parameter
  * @argv: This is the seond parameter
- * Return: Result
+ * Return: if number of arg is wrong print error + NL +exit with 98
+ *         if operator != any of the 5, print error + NL + exit with 99
+ *         if divisor  = 0 print error  + NL + exit with 100
  */
 int main(int argc, char *argv[])
 {
 	int a, b, result;
-	char *op;
+	char *my_op;
 
-	if (argc != 4)
-		return (-1);
+	if (argc < 4 || argc > 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
 
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
-	op = argv[2];
-	/* c = call get_op to get the actual operator*/
-	result = get_op_func(op)(a, b);
+	my_op = argv[2];
 
+	if (b == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
+
+	if (*my_op != '+' && *my_op != '-' && *my_op != '/' &&
+			*my_op != '*' && *my_op != '%')
+	{
+		printf("Error\n");
+		return (99);
+	}
+	/* c = call get_op to get the actual operator*/
+	result = get_op_func(my_op)(a, b);
 	printf("%d\n", result);
 	return (0);
 }

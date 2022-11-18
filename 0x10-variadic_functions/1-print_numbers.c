3c5355959b9ac6103a004f9161b197362fae140e
Ezra-Mallo <ezra.mallo@gmail.com>
2022-11-18 01:45:34 +0100

    Task 2: Write a function that prints strings, followed by a new line. Prototype: void print_strings(const char *separator, const unsigned int n, ...); where separator is the string to be printed between the strings and n is the number of strings passed to the function You are allowed to use printf If separator is NULL, don’t print it If one of the string is NULL, print (nil) instead Print a new line at the end of your function Repo: GitHub repository: alx-low_level_programming Directory: 0x10-variadic_functions File: 2-print_strings.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,7 +13,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 
 	va_start(ap, n);
-	if (separator != NULL)
+	if (separator != NULL && strlen(separator) != 0 && n != 0)
 	{
 		for (i = 0; i < n; i++)
 		{

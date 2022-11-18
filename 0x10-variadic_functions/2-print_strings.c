ff9c550a9e43892eb7452f972092fb727bc2e31d
Ezra-Mallo <ezra.mallo@gmail.com>
2022-11-18 01:54:39 +0100

    Task 2: Write a function that prints strings, followed by a new line. Prototype: void print_strings(const char *separator, const unsigned int n, ...);

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,7 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			if (strlen(buf) == 0 || buf == NULL)
 				buf = "nil";
 			printf("%s", buf);
-			if (i != (n - 1))
+			if (i != (n - 1) && separator != NULL)
 				printf("%s", separator);
 		}
 		va_end(str);

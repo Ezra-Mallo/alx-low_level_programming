1acc7cab284fab8bab5a4d2d949032315ab34ae8
Ezra-Mallo <ezra.mallo@gmail.com>
2022-11-25 10:06:36 +0100

    Write a function that adds a new node at the beginning of a list_t list.

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * add_node_end - This function adds data to the end of single linked list
+ * @head: This is the list header
+ * @str: This is the string to the added
+ * Return: This retruend an unsigned int of number of nodes
+ *If str is NULL, print [0] (nil)
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *newNode;
+
+	newNode = malloc(sizeof(list_t));
+
+	newNode->str = (char *)(strdup(str));
+	newNode->len = strlen(str);
+	newNode->next = NULL;
+
+	if (*head == NULL)
+		*head = newNode;
+	else
+	{
+		list_t *lastNode = *head;
+
+		while (lastNode->next != NULL)
+		{
+			lastNode = lastNode->next;
+		}
+		lastNode->next = newNode;
+		return (lastNode);
+	}
+	return (*head);
+}

3982ae3c71e1001b05fad3ae9d9d0076f1417e3d
Ezra-Mallo <ezra.mallo@gmail.com>
2022-11-28 00:01:51 +0100

    Task 4- Write a function that prints acstrig before the main function is executed. Task 5- Write a function that frees a list_t list.

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,8 +9,7 @@ void my_printf_constructor(void) __attribute__ ((constructor));
  */
 int main(void)
 {
-	printf("(A tortoise, having pretty good sense of a hare's nature,"
-			"challenges one to a race.)\n");
+	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
 	return (0);
 }
 
@@ -20,6 +19,5 @@ int main(void)
  * **/
 void my_printf_constructor(void)
 {
-	printf("You're beat! and yet, you must allow,\n"
-			"I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }

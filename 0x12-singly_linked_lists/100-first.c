027671dfb4851c6ffbc0623d7c95e02be167f1f6
Ezra-Mallo <ezra.mallo@gmail.com>
2022-11-28 09:46:44 +0100

    Task 5- Write a function that prints Youre beat and yet, you must allow I bore my house upon my back before the main function is executed.

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,23 +1,13 @@
 #include <stdio.h>
+void constructor_run_b4_main_function(void) __attribute__((constructor));
 
-void my_printf_constructor(void) __attribute__ ((constructor));
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * constructor_run_b4_main_function - this is the constructor that executes
+ *                      before the main fucntion
  */
-int main(void)
+void constructor_run_b4_main_function(void)
 {
-	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-	return (0);
-}
-
-
-/**
- * my_printf_constructor - this is the constructor
- * **/
-void my_printf_constructor(void)
-{
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
 }

75bddc1ac3368fb4cbd730cd5c8f2895d1290abc
Ezra-Mallo <ezra.mallo@gmail.com>
2022-11-29 23:02:49 +0100

    Task 0- 14 on Singly linked list.

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -16,15 +16,15 @@ size_t print_listint(const listint_t *h)
 	my_print_list = h;
 
 
-	if (my_print_list == NULL)
-		return (0);
-
-	while (my_print_list != NULL)
+	if (my_print_list != NULL)
 	{
-		printf("%d\n", my_print_list->n);
-		my_print_list = my_print_list->next;
-		i++;
+		while (my_print_list != NULL)
+		{
+			printf("%d\n", my_print_list->n);
+			my_print_list = my_print_list->next;
+			i++;
+		}
+		return (i);
 	}
-
-	return (i);
+	return (0);
 }

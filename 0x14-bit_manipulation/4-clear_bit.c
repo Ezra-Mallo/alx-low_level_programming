0570b68cf491f3ace050348122b3cb3f181e8e9f
Ezra-Mallo <ezra.mallo@gmail.com>
2022-12-02 03:55:14 +0100

    Task 0 - 7

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * clear_bit - Write a function that sets the value of a bit to 0 at a given index
+ * @n: is the parameter for the decimal number to be converted to binary
+ * @index: 2nd parameter
+ * Return: int value of the index
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int val_div, i, result = 0, val_of_n;
+	int remender, bit, power, j, k, dest[50];
+
+	val_of_n = *n;
+
+	if (n == 0)
+	{
+		dest[0] = 0;
+		i = 1;
+		return (-1);
+	}
+	val_div = 1;
+	i = 0;
+	while (val_div != 0)
+	{
+		remender = val_of_n % 2;
+		val_div = val_of_n / 2;
+		if (i == index)
+			dest[i] = 0;
+		else
+			dest[i] = remender;
+		val_of_n = val_div;
+		i++;
+	}
+	for (k = i - 1; k >= 0; k--)
+	{
+		bit = dest[k];
+		power = 1;
+		for (j = 0; j < k; j++)
+			power = power * 2;
+		result += power * bit;
+	}
+	*n = result;
+	return (1);
+}

bfe3c2b19aa6e66765e60368791a2ecd8bd467bf
Ezra-Mallo <ezra.mallo@gmail.com>
2022-12-06 00:13:43 +0100

    Task 0 - 1 of C: file I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,22 +12,21 @@ int create_file(const char *filename, char *text_content)
 {
 	int  len = 0, w_count, fd;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
+
 
 	while (text_content[len] != '\0')
 		len++;
 
 
-	if (len == 0)
-		return (-1);
-
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-
 	if (fd == -1)
 		return (-1);
-
-	w_count = write(fd, text_content, len);
+	if (len != 0)
+		w_count = write(fd, text_content, len);
+	else
+		w_count = write(fd, "", 0);
 
 	if (w_count == -1)
 		return (-1);
--- 0x15-file_io/1-create_file_bkup.c
+++ 0x15-file_io/1-create_file_bkup.c
@@ -2,23 +2,21 @@
 #include <stdio.h>
 
 /**
- * append_text_to_file - a function that appends text at the end of a file
+ * create_file - Create a function that creates a file
  * @filename: 1st parameter
  * @text_content: 2nd parameter
  *
  * Return: Number of characters
  */
-int append_text_to_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 	int  len = 0, w_count, fd;
 
+	if (filename == NULL)
+		return (-1);
+
 	if (text_content == NULL)
-	{
-		if (filename == NULL)
-			return (-1);
-		else
-			return (1);
-	}
+		exit (1);
 
 	while (text_content[len] != '\0')
 		len++;
@@ -27,7 +25,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (len == 0)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_APPEND);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);

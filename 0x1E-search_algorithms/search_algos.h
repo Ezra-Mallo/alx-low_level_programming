c2a2ec4600480fa4fda44c3eb629828347996259
Ezra-Mallo <ezra.mallo@gmail.com>
2023-07-13 00:54:00 +0100

    Added Task 7. Jump search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,9 +1,11 @@
 #ifndef _SEARCH_HEADER_
 #define _SEARCH_HEADER_
+
 #include <stddef.h>
 #include <stdio.h>
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-
+int jump_search(int *array, size_t size, int value);
 #endif

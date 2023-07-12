ed4941c02c94eb2e279e567bec268db571444ab1
Ezra-Mallo <ezra.mallo@gmail.com>
2023-07-13 00:35:02 +0100

    updated Task 0

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,9 @@
 #ifndef _SEARCH_HEADER_
 #define _SEARCH_HEADER_
-int linear_search(int *array, size_t size, int value)
-int binary_search(int *array, size_t size, int value)
+#include <stddef.h>
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
 
 #endif
